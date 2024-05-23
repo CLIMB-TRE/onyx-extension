da1df3dff2409926364562ce8e2d3a407019cd91
n.a.butcher@bham.ac.uk <n.a.butcher@bham.ac.uk>
2024-05-23 10:11:56 +0100

    using a relative path for the icon resource

--- src/icon.ts
+++ src/icon.ts
@@ -1,5 +1,5 @@
 import { LabIcon } from '@jupyterlab/ui-components';
-import CHAT_ICON from './style/icons/dna.svg';
+import CHAT_ICON from './../style/icons/dna.svg';
 
 export const chatIcon = new LabIcon({
   name: 'jitsi:chat',
