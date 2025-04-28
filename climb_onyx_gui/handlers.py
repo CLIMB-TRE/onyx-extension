93b1a00d8009fda0fad2c371169624bcf17b6245
tombch <t.brier@outlook.com>
2025-04-28 15:09:57 +0100

    Updating API handlers with standardised 4XX status codes for exceptions. Also removed tempfile logic for S3ViewHandler as this is now unsupported

--- climb_onyx_gui/handlers.py
+++ climb_onyx_gui/handlers.py
@@ -1,124 +1,152 @@
 import os
 import json
 import re
-import requests
-import tempfile
 from pathlib import Path
-
-from jupyter_server.base.handlers import APIHandler
-from jupyter_server.utils import url_path_join
+import requests
 import tornado
 import boto3
+from jupyter_server.base.handlers import APIHandler
+from jupyter_server.utils import url_path_join
 from ._version import __version__
+from .exceptions import APIError, ValidationError, AuthenticationError
+
 
 class S3ViewHandler(APIHandler):
-
-    def _copy_s3_file(self, s3name:str)->str:
-        m = re.match(r's3://(.*)/(.*)', s3name)
-        b = m.group(1)
-        o = m.group(2)
-
-        s3 = boto3.resource(
-            "s3",
-            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-            endpoint_url=os.environ["JUPYTERLAB_S3_ENDPOINT"],
-            )
-        s3_object=s3.Object(b,o)
-        Path("./s3_downloads").mkdir(parents=True, exist_ok=True)
-        with open(f'./s3_downloads/{o}', 'wb') as fp:
-            s3_object.download_fileobj(fp)
-        return f'./s3_downloads/{o}'    
-
     @tornado.web.authenticated
     def get(self):
         try:
-            
-            s3location = self.get_query_argument("s3location")
-            temp_file = self._copy_s3_file(s3location)
-            self.finish(json.dumps({
-                "location": s3location,
-                "temp_file": temp_file
-            }))
-        except Exception as e:
-            self.finish(json.dumps({
-                "exception": e
-            }))
+            # Validate S3 URI
+            uri = self.get_query_argument("uri")
 
+            if not uri:
+                raise ValidationError("S3 URI is required")
+
+            match = re.match(r"s3://(.*)/(.*)", uri)
+
+            if not match:
+                raise ValidationError(f"Invalid S3 URI: {uri}")
+
+            bucket_name = match.group(1)
+            key = match.group(2)
+
+            if not bucket_name:
+                raise ValidationError(f"S3 bucket name not found: {uri}")
+
+            if not key:
+                raise ValidationError(f"S3 key not found: {uri}")
+
+            # Retrieve S3 object
+            s3 = boto3.resource(
+                "s3",
+                aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+                aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+                endpoint_url=os.environ["JUPYTERLAB_S3_ENDPOINT"],
+            )
+            s3_object = s3.Object(bucket_name=bucket_name, key=key)  # type: ignore
+
+            # Create directory to store object (if it doesn't exist)
+            Path("./s3_downloads").mkdir(parents=True, exist_ok=True)
+
+            # Download the object
+            path = f"./s3_downloads/{key}"
+            with open(path, "wb") as fp:
+                s3_object.download_fileobj(fp)
+
+            self.finish(json.dumps({"path": path}))
+
+        except APIError as e:
+            self.set_status(e.STATUS_CODE)
+            self.finish(json.dumps({"message": str(e)}))
 
 
 class RedirectingRouteHandler(APIHandler):
     @tornado.web.authenticated
     def get(self):
         try:
-            
-            domain = os.environ.get('ONYX_DOMAIN', '*Unknown*').strip('/')
-            token = os.environ.get('ONYX_TOKEN', '*Unknown*')
-            route_extension = self.get_query_argument("route")
-            route = f"{domain}/{route_extension}"
-            r= requests.get(route, headers={"Authorization": f"Token {token}"})
-            self.finish(r.content)
-        except Exception as e:
-            self.finish(json.dumps({
-                "exception": e
-            }))
+            # Validate domain
+            domain = os.environ.get("ONYX_DOMAIN")
+
+            if not domain:
+                raise AuthenticationError("Domain is required to connect to Onyx")
+            else:
+                domain = domain.removesuffix("/")
+
+            # Validate token
+            token = os.environ.get("ONYX_TOKEN")
+
+            if not token:
+                raise AuthenticationError("Token is required to connect to Onyx")
+
+            # Make the request and return the response
+            route = self.get_query_argument("route")
+            endpoint = f"{domain}/{route}"
+            response = requests.get(
+                endpoint, headers={"Authorization": f"Token {token}"}
+            )
+            self.finish(response.content)
+
+        except APIError as e:
+            self.set_status(e.STATUS_CODE)
+            self.finish(json.dumps({"message": str(e)}))
 
 
 class VersionHandler(APIHandler):
     @tornado.web.authenticated
     def get(self):
         try:
-            self.finish(json.dumps({
-                "version": __version__,
-            }))
-        except Exception as e:
-            self.finish(json.dumps({
-                "exception": e
-            }))
+            self.finish(json.dumps({"version": __version__}))
+        except APIError as e:
+            self.set_status(e.STATUS_CODE)
+            self.finish(json.dumps({"message": str(e)}))
 
 
 class FileWriteHandler(APIHandler):
     @tornado.web.authenticated
     def post(self):
         try:
+            # Validate path
             path = self.get_query_argument("path")
+
+            if not path:
+                raise ValidationError("Path is required")
+
+            # Validate content
             input_data = self.get_json_body()
+            if not input_data:
+                raise ValidationError("Input data is required")
+
             content = input_data["content"]
+
+            # Create directory to store content (if it doesn't exist)
             Path(path).parent.mkdir(parents=True, exist_ok=True)
-            with open(path, 'w', encoding="utf-8") as fp:
+
+            # Write content to file
+            with open(path, "w", encoding="utf-8") as fp:
                 fp.write(content)
-            self.finish(json.dumps({
-                "path": path,
-            }))
-            
-        except Exception as e:
-            self.finish(json.dumps({
-                "exception": e
-            }))
+
+            self.finish(json.dumps({"path": path}))
+
+        except APIError as e:
+            self.set_status(e.STATUS_CODE)
+            self.finish(json.dumps({"message": str(e)}))
 
 
 def setup_handlers(web_app):
-    tempfile.mkdtemp()
     host_pattern = ".*$"
-
     base_url = web_app.settings["base_url"]
 
     route_pattern = url_path_join(base_url, "climb-onyx-gui", "s3")
     handlers = [(route_pattern, S3ViewHandler)]
     web_app.add_handlers(host_pattern, handlers)
 
-    
     route_pattern = url_path_join(base_url, "climb-onyx-gui", "reroute")
     handlers = [(route_pattern, RedirectingRouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
 
-    
     route_pattern = url_path_join(base_url, "climb-onyx-gui", "version")
     handlers = [(route_pattern, VersionHandler)]
     web_app.add_handlers(host_pattern, handlers)
 
-    
     route_pattern = url_path_join(base_url, "climb-onyx-gui", "file-write")
     handlers = [(route_pattern, FileWriteHandler)]
     web_app.add_handlers(host_pattern, handlers)
-
