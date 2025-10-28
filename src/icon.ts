b7bc1a0d31bd75d449e540169dd6846ab2e7e4c5
tombch <t.brier@outlook.com>
2025-10-28 17:14:29 +0000

    Updating icon names

--- src/icon.ts
+++ src/icon.ts
@@ -3,11 +3,11 @@ import DOCS_ICON from './../style/icons/docs.svg';
 import ONYX_ICON from './../style/icons/onyx.svg';
 
 export const docsIcon = new LabIcon({
-  name: 'climb-onyx-gui:dna',
+  name: 'climb-onyx-gui:docs',
   svgstr: DOCS_ICON
 });
 
 export const onyxIcon = new LabIcon({
-  name: 'climb-onyx-gui:inner-join',
+  name: 'climb-onyx-gui:onyx',
   svgstr: ONYX_ICON
 });
